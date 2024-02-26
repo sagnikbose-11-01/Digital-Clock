9e58d0b8831b455d5c161f2373d91cf84184c4d6
Sagnik Bose <143270398+sagnikbose-11-01@users.noreply.github.com>
2024-02-26 13:08:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ setInterval(() => {
 
     // console.log(currentTime)
 
-    hrs.innerHTML = currentTime.getHours();
+    hrs.innerHTML = (currentTime.getHours()<10?'0':'') + currentTime.getHours();
     min.innerHTML = (currentTime.getMinutes()<10?'0':'') + currentTime.getMinutes();
     sec.innerHTML = (currentTime.getSeconds()<10?'0':'') + currentTime.getSeconds();
 }, 1000);
